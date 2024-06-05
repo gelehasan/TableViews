45a1dd66f63e31af3778195b157665dc42f6ec38
Gele hasan <gellehussen@gmail.com>
2024-06-05 10:50:49 +0200

    table view

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, SafeAreaView, ScrollView} from 'react-native';
+import { Cell, Section, TableView } from 'react-native-tableview-simple';
 export default function App() {
+
+  let tableData = [{"title":"Row 1"}, {"title":"Row 2"}] ;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //For the content to not touch the top notch
+    <SafeAreaView> 
+       {/** for scrolling behavior */}
+      <ScrollView style={{height:"100%"}}> 
+    <TableView>
+      <Section>
+        {
+          tableData.map((x)=>(
+            <Cell
+                title={x.title}
+            />
+          ))
+        }
+
+      </Section>
+    </TableView>
+    </ScrollView>
+    </SafeAreaView>
+
   );
 }
 
